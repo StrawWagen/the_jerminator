684664ad99cddad54171fcc194df5cfbad86f7d7
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2024-09-13 21:57:30 -0700

    Only do punching stance when ready to attack.

--- lua/weapons/weapon_jerminator_fists.lua
+++ lua/weapons/weapon_jerminator_fists.lua
@@ -20,3 +20,47 @@ else
 
     end
 end
+
+function SWEP:HoldTypeThink()
+    local owner = self:GetOwner()
+    if not IsValid( owner ) then return end
+    if not owner.GetEnemy then return end
+    local enemy = owner:GetEnemy()
+    local holdType = "normal"
+    local doFistsTime = self.doFistsTime
+
+    local path = owner:GetPath()
+
+    if doFistsTime > CurTime() then
+        holdType = "fist"
+
+    elseif owner:IsReallyAngry() then
+        holdType = "fist"
+
+    elseif IsValid( enemy ) and enemy.isTerminatorHunterKiller then
+        holdType = "fist"
+        doFistsTime = math.max( doFistsTime, CurTime() + 10 )
+
+    elseif IsValid( enemy ) and owner.DistToEnemy and owner.DistToEnemy < self.Range * 4 then
+        holdType = "fist"
+
+    elseif owner:getLostHealth() > 0.01 then
+        holdType = "fist"
+        doFistsTime = math.max( doFistsTime, CurTime() + 10 )
+
+    elseif IsValid( enemy ) and path and path:GetEnd() and path:GetEnd():DistToSqr( enemy:GetPos() ) < 1000^2 and path:GetLength() < 1000 then
+        holdType = "fist"
+        doFistsTime = math.max( doFistsTime, CurTime() + 3 )
+
+    end
+
+    self.doFistsTime = doFistsTime
+    local oldHoldType = self.oldHoldType
+
+    if not oldHoldType or oldHoldType ~= holdType then
+        self:SetHoldType( holdType )
+        print( "blarg")
+        self.oldHoldType = holdType
+
+    end
+end
